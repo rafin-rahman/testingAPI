34978cb6ddfc3ed6ee0360a64a7f8c7b592c56e5
rafin <ra-fin@live.com>
2023-09-10 23:55:34 +0100

    testing api

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import UserList from "@/components/UserList";
 
 export default function Home() {
   return (
-    <>
+    <main style={{ maxWidth: 1200, marginInline: "auto", padding: 20 }}>
       <UserList />
-    </>
+    </main>
   );
 }
--- components/UserList.tsx
+++ components/UserList.tsx
@@ -1,11 +1,29 @@
+"use client";
+
 import { user } from "@prisma/client";
-import React, { cache, use } from "react";
+import React, { cache, use, useState } from "react";
 
 const getUsers = cache(() =>
   fetch("http://localhost:3000/api/users").then((res) => res.json())
 );
+
+const deleteUser = cache((id: number) =>
+  fetch(`http://localhost:3000/api/users/${id}`, {
+    method: "DELETE",
+  }).then((res) => res.json())
+);
+
 export default function ListUsers() {
+  const editUser = cache((id: number) =>
+    fetch(`http://localhost:3000/api/users/${id}`, {
+      method: "PATCH",
+      body: JSON.stringify({ name: userName }),
+    }).then((res) => res.json())
+  );
+
   let users = use<user[]>(getUsers());
+  const [userName, setUserName] = useState("");
+  const [userList, setUserList] = useState(users);
 
   return (
     <div
@@ -15,7 +33,7 @@ export default function ListUsers() {
         gap: 20,
       }}
     >
-      {users.map((user) => (
+      {userList.map((user) => (
         <div
           key={user.id}
           style={{ border: "1px solid #ccc", textAlign: "center" }}
@@ -26,6 +44,35 @@ export default function ListUsers() {
             style={{ height: 180, width: 180 }}
           />
           <h3>{user.name}</h3>
+          <button
+            className={
+              "bg-yellow-400 hover:bg-yellow-500 text-white font-bold py-2 px-4  shadow-md transform hover:scale-105 transition duration-300"
+            }
+            onClick={() => {
+              deleteUser(user.id);
+            }}
+          >
+            Delete
+          </button>
+          <br />
+          <br />
+          <button
+            className={
+              "bg-orange-400 hover:bg-orange-500 text-white font-bold py-2 px-4  shadow-md transform hover:scale-105 transition duration-300"
+            }
+            onClick={() => {
+              editUser(user.id);
+            }}
+          >
+            Edit
+          </button>
+          <input
+            className={
+              "w-full px-4 py-2 rounded-lg border border-gray-300 focus:outline-none focus:border-blue-500 placeholder-gray-400"
+            }
+            type="text"
+            onChange={(e) => setUserName(e.target.value)}
+          />
         </div>
       ))}
     </div>
